43a6f0cbe6a8ccfca695a2a4ef4591dac666f738
jojimanos <manosgeorge7@gmail.com>
2023-08-31 12:10:49 +0300

    exercises 7.4-7.6

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from "../hooks"
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.data.value,
+      author: author.data.value,
+      info: info.data.value,
       votes: 0
     })
     navigate('/')
+  }
+
+  const handleClear = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
@@ -25,17 +33,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content.data} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author.data} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info.data} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleClear}>clear</button>
       </form>
     </div>
   )
---
+++ routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const reset = () => {
+        setValue('')
+    }
+
+    const data = {value, onChange}
+
+    return {
+        type,
+        value,
+        data,
+        reset
+    }
+}
